7e14a2130bdda2afc28b6d6e16cfcca2f83ed75f
LenoMac <129600379+LenoMac@users.noreply.github.com>
2023-05-18 18:21:07 +0600

    added some functions

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
 const apiKey = "b8de58bdd36f820d25bcfeee4e137586"
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bishkek"
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+
+const searchInput = document.getElementById('search')
+const searchBtn = document.getElementById('btn')
+const weatherIcon = document.getElementById('weatherIcon')
 
 
-async function checkWeather(){
-    const response = await fetch(apiURL + `&appid=${apiKey}`);
-    var data = await response.json();
+async function current() {
 
-    console.log(data);
 }
 
-checkWeather()
+
+async function checkWeather(city) {
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`)
+    var data = await response.json()
+
+    if (response.status == 404) {
+        document.getElementById('error').style.display = 'block'
+        document.getElementById('hidden').style.display = 'none'
+    } else {
+        console.log(data)
+        document.getElementById('city').innerHTML = data.name
+        document.getElementById('temp').innerHTML = Math.round(data.main.temp) + '°C'
+        document.getElementById('humidity').innerHTML = data.main.humidity + '%'
+        document.getElementById('wind').innerHTML = data.wind.speed + ' km/h'
+
+        let status = data.weather[0].main
+        if (status === 'Clouds') {
+            weatherIcon.src = './img/clouds.png'
+        } else if (status === 'Clear') {
+            weatherIcon.src = './img/clear.png'
+        } else if (status === 'Rain') {
+            weatherIcon.src = './img/mist.png'
+        } else if (status === 'Snow') {
+            weatherIcon.src = './img/snow.png'
+        } else if (status === 'Drizzle') {
+            weatherIcon.src = './img/drizzle.png'
+        }
+        searchInput.value = ""
+        document.getElementById('error').style.display = 'none'
+    }
+
+}
+searchBtn.addEventListener('click', () => {
+    checkWeather(searchInput.value)
+    document.getElementById('hidden').style.display = 'block'
+})
